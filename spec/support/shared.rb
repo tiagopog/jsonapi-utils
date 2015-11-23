90a7793d4bcbac448d9942a73dfb68418602c226
Tiago Guedes <tiagopog@gmail.com>
2015-11-23 00:17:15 -0200

    Dry up some error specs

--- spec/support/shared.rb
+++ spec/support/shared.rb
@@ -1,89 +1,71 @@
 shared_examples_for 'request with error' do |options|
+  shared_examples_for '400 response' do |options|
+    it 'renders 400 response' do
+      record = options[:record]
+      params = record.nil? ? {} : { :"#{record[:key]}" => 1 }
+      params.merge!(options[:params])
+
+      get options[:action], params, headers
+
+      expect(response).to have_http_status 400
+      expect(json['errors'][0]['title']).to eq(options[:expect][:title])
+      expect(json['errors'][0]['code']).to eq(options[:expect][:code])
+    end
+  end
+
   context 'with an invalid query string parameter' do
     context 'when error is in "fields"' do
       context 'with an invalid resource name' do
-        it 'renders 400 response' do
-          record = options[:record]
-          params = record.nil? ? {} : { :"#{record[:key]}" => 1 }
-          params.merge!(fields: { foo: 'name' })
-
-          get options[:action], params, headers
-
-          expect(response).to have_http_status 400
-          expect(json['errors'][0]['title']).to eq('Invalid resource')
-          expect(json['errors'][0]['code']).to eq(101)
-        end
+        options.merge!({
+          params: { fields: { foo: 'name' } },
+          expect: { title: 'Invalid resource', code: 101 }
+        })
+        it_behaves_like '400 response', options
       end
 
       context 'with an invalid field' do
-        it 'renders 400 response' do
-          record = options[:record]
-          params = record.nil? ? {} : { :"#{record[:key]}" => 1 }
-          params.merge!(fields: { users: 'foo' })
-
-          get options[:action], params, headers
-
-          expect(response).to have_http_status 400
-          expect(json['errors'][0]['title']).to eq('Invalid field')
-          expect(json['errors'][0]['code']).to eq(104)
-        end
+        options.merge!({
+          params: { fields: { users: 'foo' } },
+          expect: { title: 'Invalid field', code: 104 }
+        })
+        it_behaves_like '400 response', options
       end
     end
 
     context 'when error is in "include"' do
-      it 'renders 400 response' do
-        record = options[:record]
-        params = record.nil? ? {} : { :"#{record[:key]}" => 1 }
-        params.merge!(include: 'foo')
-
-        get options[:action], params, headers
-
-        expect(response).to have_http_status 400
-        expect(json['errors'][0]['title']).to eq('Invalid field')
-        expect(json['errors'][0]['code']).to eq(112)
-      end
+      options.merge!({
+        params: { include: 'foo' },
+        expect: { title: 'Invalid field', code: 112 }
+      })
+      it_behaves_like '400 response', options
     end
   end
 
   if options[:action] == :index
     context 'when error is in "filter"' do
-      it 'renders 400 response' do
-        get options[:action], { filter: { foo: 'bar' } }, headers
-
-        expect(response).to have_http_status 400
-        expect(json['errors'][0]['title']).to eq('Filter not allowed')
-        expect(json['errors'][0]['code']).to eq(102)
-      end
+      options.merge!({
+        params: { filter: { foo: 'bar' } },
+        expect: { title: 'Filter not allowed', code: 102 }
+      })
+      it_behaves_like '400 response', options
     end
 
     context 'when error is in "sort"' do
-      it 'renders 400 response' do
-        get options[:action], { sort: 'foo' }, headers
-
-        expect(response).to have_http_status 400
-        expect(json['errors'][0]['title']).to eq('Invalid sort criteria')
-        expect(json['errors'][0]['code']).to eq(114)
-      end
+      options.merge!({
+        params: { sort: 'foo' },
+        expect: { title: 'Invalid sort criteria', code: 114 }
+      })
+      it_behaves_like '400 response', options
     end
 
     context 'when error is in "page"' do
-      context 'with invalid "number"' do
-        it 'renders 400 response' do
-          get options[:action], { page: { number: 'foo' } }, headers
-
-          expect(response).to have_http_status 400
-          expect(json['errors'][0]['title']).to eq('Invalid page value')
-          expect(json['errors'][0]['code']).to eq(118)
-        end
-      end
-
-      context 'with invalid "size"' do
-        it 'renders 400 response' do
-          get options[:action], { page: { size: 'foo' } }, headers
-
-          expect(response).to have_http_status 400
-          expect(json['errors'][0]['title']).to eq('Invalid page value')
-          expect(json['errors'][0]['code']).to eq(118)
+      %i(number size).each do |field|
+        context 'with invalid "number"' do
+          options.merge!({
+            params: { page: { "#{field}": 'foo' } },
+            expect: { title: 'Invalid page value', code: 118 }
+          })
+          it_behaves_like '400 response', options
         end
       end
     end
