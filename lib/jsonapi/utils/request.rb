7f12fbd6299258d683178ec218000906c4d23efa
Tiago Guedes <tiagopog@gmail.com>
2016-07-02 23:33:49 -0300

    Add JSONAPI::Utils::Request#relationship_params helper method

--- lib/jsonapi/utils/request.rb
+++ lib/jsonapi/utils/request.rb
@@ -16,9 +16,27 @@ module JSONAPI
       end
 
       def resource_params
-        unless @request.operations.empty?
-          operation = @request.operations.find { |e| e.data[:attributes].present? }
-          operation.nil? ? {} : operation.data[:attributes]
+        build_params_for(:resource)
+      end
+
+      def relationship_params
+        build_params_for(:relationship)
+      end
+
+      private
+
+      def build_params_for(param_type)
+        return {} if @request.operations.empty?
+
+        keys      = %i(attributes to_one to_many)
+        operation = @request.operations.find { |e| e.data.keys & keys == keys }
+
+        if operation.nil?
+          {}
+        elsif param_type == :relationship
+          operation.data.values_at(:has_one, :to_many).compact.reduce(&:merge)
+        else
+          operation.data[:attributes]
         end
       end
     end
