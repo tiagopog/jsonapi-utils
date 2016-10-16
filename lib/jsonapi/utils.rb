d13b57991bcbe9f854475197e870fc30365ec435
Tiago Guedes <tiagopog@gmail.com>
2016-10-15 21:13:56 -0300

    Add support for custom paginators & fix test cases

--- lib/jsonapi/utils.rb
+++ lib/jsonapi/utils.rb
@@ -1,8 +1,12 @@
+
 require 'jsonapi-resources'
 require 'jsonapi/utils/version'
 require 'jsonapi/utils/exceptions'
 require 'jsonapi/utils/request'
 require 'jsonapi/utils/response'
+require 'jsonapi/utils/support/filter/custom'
+
+JSONAPI::Resource.extend JSONAPI::Utils::Support::Filter::Custom
 
 module JSONAPI
   module Utils
@@ -10,6 +14,8 @@ module JSONAPI
     include Response
 
     def self.included(base)
+      base.include ActsAsResourceController
+
       if base.respond_to?(:before_action)
         base.before_action :jsonapi_request_handling
       end
---
+++ spec/support/paginators.rb
@@ -0,0 +1,7 @@
+class CustomOffsetPaginator < OffsetPaginator
+  def pagination_range(page_params)
+    offset = page_params['offset'].to_i.nonzero? || 0
+    limit  = page_params['limit'].to_i.nonzero?  || JSONAPI.configuration.default_page_size
+    offset..offset + limit - 1
+  end
+end
