f0377d72440edebb3bffb435cc3e24db9accec67
Chris Peters <chris@minimalorange.com>
2016-10-18 22:54:25 -0400

    Touch-ups to AR error logic

--- lib/jsonapi/utils/exceptions.rb
+++ lib/jsonapi/utils/exceptions.rb
@@ -4,58 +4,87 @@ module JSONAPI
   module Utils
     module Exceptions
       class ActiveRecord < ::JSONAPI::Exceptions::Error
-        attr_accessor :object, :resource, :relationships, :relationship_keys, :foreign_keys
+        attr_reader :object, :resource, :relationships, :relationship_keys, :foreign_keys
 
-        def initialize(object, request, context)
+        def initialize(object, resource_klass, context)
           @object = object
-          request = request
-          resource_klass = request.resource_klass
           @resource = resource_klass.new(object, context)
 
-          # Need to reflect on object's associations for error reporting.
+          # Need to reflect on resource's relationships for error reporting.
           @relationships     = resource_klass._relationships.values
           @relationship_keys = @relationships.map(&:name).map(&:to_sym)
           @foreign_keys      = @relationships.map(&:foreign_key).map(&:to_sym)
         end
 
         def errors
-          object.errors.keys.map do |key|
-            error_meta = {
-              code: JSONAPI::VALIDATION_ERROR,
-              status: :unprocessable_entity,
-              title: object.errors.full_messages_for(key).first
-            }
+          object.errors.messages.flat_map do |key, messages|
+            messages.map do |message|
+              error_meta = error_base
+                .merge(title: title_member(key, message))
+                .merge(id: id_member(key))
+                .merge(source_member(key))
 
-            # Determine if this is a foreign key, which will need to look up its
-            # matching association name.
-            is_foreign_key = foreign_keys.include?(key)
-            id = is_foreign_key ? relationships.select { |r| r.foreign_key == key }.first.name : key
-            id = id.to_sym
+              JSONAPI::Error.new(error_meta)
+            end
+          end
+        end
 
-            key_formatter = JSONAPI.configuration.key_formatter
-            error_meta[:id] = key_formatter.format(id).to_sym
+        private
+
+        def id_member(key)
+          id = resource_key_for(key)
+          key_formatter = JSONAPI.configuration.key_formatter
+          key_formatter.format(id).to_sym
+        end
+
+        # Determine if this is a foreign key, which will need to look up its
+        # matching association name.
+        def resource_key_for(key)
+          if foreign_keys.include?(key)
+            relationships.find { |r| r.foreign_key == key }.name.to_sym
+          else
+            key
+          end
+        end
+
+        def source_member(key)
+          Hash.new.tap do |hash|
+            resource_key = resource_key_for(key)
 
             # Pointer should only be created for whitelisted attributes.
-            if resource.fetchable_fields.include?(id) || key == :base
-              error_meta[:source] = {}
+            return hash unless resource.fetchable_fields.include?(resource_key) || key == :base
 
-              # Pointer depends on whether we're using an association, foreign
-              # key, base, or attribute.
-              error_meta[:source][:pointer] =
-                # Relationship
-                if is_foreign_key || relationship_keys.include?(id)
-                  "/data/relationships/#{error_meta[:id]}"
-                # Base
-                elsif key == :base
-                  '/data'
-                # Attribute
-                else
-                  "/data/attributes/#{error_meta[:id]}"
-                end
-            end
+            id = id_member(key)
 
-            JSONAPI::Error.new(error_meta)
+            hash[:source] = {}
+            hash[:source][:pointer] =
+              # Relationship
+              if relationship_keys.include?(resource_key)
+                "/data/relationships/#{id}"
+              # Base
+              elsif key == :base
+                '/data'
+              # Attribute
+              else
+                "/data/attributes/#{id}"
+              end
           end
+        end
+
+        def title_member(key, message)
+          if key == :base
+            message
+          else
+            resource_key = resource_key_for(key)
+            [resource_key.to_s.tr('.', '_').humanize, message].join(' ')
+          end
+        end
+
+        def error_base
+          {
+            code: JSONAPI::VALIDATION_ERROR,
+            status: :unprocessable_entity
+          }
         end
       end
 
--- lib/jsonapi/utils/response/formatters.rb
+++ lib/jsonapi/utils/response/formatters.rb
@@ -14,7 +14,7 @@ module JSONAPI
         alias_method :jsonapi_serialize, :jsonapi_format
 
         def jsonapi_format_errors(data)
-          data   = JSONAPI::Utils::Exceptions::ActiveRecord.new(data, @request, context) if data.is_a?(ActiveRecord::Base)
+          data   = JSONAPI::Utils::Exceptions::ActiveRecord.new(data, @request.resource_klass, context) if data.is_a?(ActiveRecord::Base)
           errors = data.respond_to?(:errors) ? data.errors : data
           JSONAPI::Utils::Support::Error.sanitize(errors).uniq
         end
