946ad1e5ec099be6644b043734af5da44ca5532c
Tiago Guedes <tiagopog@gmail.com>
2016-12-17 02:13:03 -0200

    Improve code docs

--- lib/jsonapi/utils/request.rb
+++ lib/jsonapi/utils/request.rb
@@ -1,10 +1,18 @@
 module JSONAPI::Utils
   module Request
+    # Setup and check request before action gets actually evaluated.
+    #
+    # @api public
     def jsonapi_request_handling
       setup_request
       check_request
     end
 
+    # Instantiate the request object.
+    #
+    # @return [JSONAPI::RequestParser]
+    #
+    # @api public
     def setup_request
       @request ||= JSONAPI::RequestParser.new(
         params,
@@ -14,11 +22,14 @@ module JSONAPI::Utils
       )
     end
 
+    # Render an error response if the parsed request got any error.
+    #
+    # @api public
     def check_request
       @request.errors.blank? || jsonapi_render_errors(json: @request)
     end
 
-    # Overrides the JSONAPI::ActsAsResourceController#process_request method.
+    # Override the JSONAPI::ActsAsResourceController#process_request method.
     #
     # It might be removed when the following line on JR is fixed:
     # https://github.com/cerebris/jsonapi-resources/blob/release-0-8/lib/jsonapi/acts_as_resource_controller.rb#L62
@@ -33,21 +44,38 @@ module JSONAPI::Utils
       handle_exceptions(e)
     end
 
+    # Helper to get params for the main resource.
+    #
+    # @return [Hash]
+    #
+    # @api public
     def resource_params
       build_params_for(:resource)
     end
 
+    # Helper to get params for relationship params.
+    #
+    # @return [Hash]
+    #
+    # @api public
     def relationship_params
       build_params_for(:relationship)
     end
 
     private
 
+    # Extract params from request and build a Hash with params
+    # for either the main resource or relationships.
+    #
+    # @return [Hash]
+    #
+    # @api private
     def build_params_for(param_type)
       return {} if @request.operations.empty?
 
-      keys      = %i(attributes to_one to_many)
+      keys = %i(attributes to_one to_many)
       operation = @request.operations.find { |e| e.options[:data].keys & keys == keys }
+
       if operation.nil?
         {}
       elsif param_type == :relationship
