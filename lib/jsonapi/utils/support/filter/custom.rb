55f9ae543630f6e2dc26bb0dde4a51a4761bcde5
Ruslan Khaertdinov <ruslan.khaertdinov@flatstack.com>
2017-01-20 19:06:54 +0400

    Fix custom filters assignment (#48)

--- lib/jsonapi/utils/support/filter/custom.rb
+++ lib/jsonapi/utils/support/filter/custom.rb
@@ -11,12 +11,8 @@ module JSONAPI::Utils::Support::Filter
     def custom_filter(attr)
       attr = attr.to_sym
       @_allowed_filters[attr] = {}
-
-      if !@_allowed_custom_filters.is_a?(Array)
-        @_allowed_custom_filters = Array(attr)
-      elsif @_allowed_custom_filters.include?(attr)
-        @_allowed_custom_filters.push(attr)
-      end
+      @_allowed_custom_filters ||= []
+      @_allowed_custom_filters |= [attr]
     end
   end
 end
