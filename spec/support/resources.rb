aadf39057ddea6da2217011d51e8d44f8641515b
Tiago Guedes <tiagopog@gmail.com>
2017-03-12 16:32:43 -0300

    Write a test case to cover the old issue with one-to-one relationships

--- spec/support/resources.rb
+++ spec/support/resources.rb
@@ -10,13 +10,18 @@ end
 class UserResource < JSONAPI::Resource
   attributes :first_name, :last_name, :full_name
 
+  has_one :profile, class_name: 'Profile'
   has_many :posts
 
   filters :first_name
-
   custom_filters :full_name
 
   def full_name
     "#{@model.first_name} #{@model.last_name}"
   end
+end
+
+class ProfileResource < JSONAPI::Resource
+  attributes :nickname
+  has_one :user, class_name: 'User', foreign_key: 'user_id'
 end
